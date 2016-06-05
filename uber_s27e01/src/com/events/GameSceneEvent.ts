f10f9b1d4a1052a484713601c63d01d9889f9aa2
anrainie <anrainie@163.com>
2016-06-05 15:44:47 +0800

    简化

--- uber_s27e01/src/com/events/GameSceneEvent.ts
+++
@@ -1,8 +0,0 @@
-﻿module com.events {
-    export class GameSceneEvent extends com.model.localData.event.Event {
-
-        constructor(_type: string, _data: Object) {
-            super(_type, _data);
-        }
-    }
-}   
